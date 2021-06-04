70a163afee083aa3e3dd651574cfedf07035b136
sosukesuzuki <aosukeke@gmail.com>
2021-06-04 23:33:19 +0900

    Use rollup-plugin-bundle-size

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
+import { bundleSize } from "@sosukesuzuki/rollup-plugin-bundle-size";
 import pkg from "./package.json";
 
 export default {
@@ -10,5 +11,5 @@ export default {
     { file: pkg.main, format: "cjs", exports: "auto" },
     { file: pkg.module, format: "esm" },
   ],
-  plugins: [typescript(), resolve(), commonjs(), terser()],
+  plugins: [typescript(), resolve(), commonjs(), terser(), bundleSize()],
 };
